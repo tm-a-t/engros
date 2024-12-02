909a7a5fc830abac7a879dde21022043461870a5
Artem Ivanov <art.ivanov@jetbrains.com>
2024-12-02 14:07:32 +0200

    Check

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ export default function build(html: string): Node {
 
   document.querySelectorAll('h2').forEach(h2 => {
     const p = document.createElement('p');
-    for (const { name, value } of h2.attributes) {
+    for (const {name, value} of h2.attributes) {
       p.setAttribute(name, value);
     }
     p.classList.add('layout-h2');
@@ -21,13 +21,10 @@ export default function build(html: string): Node {
   const readabilityResult = readability!.content;
   const rawContent = readabilityResult.firstChild?.firstChild!;
   const rawElements = [...rawContent.childNodes].filter(node => node.nodeType !== Node.TEXT_NODE);
-
-  const groupedElements = groupElements([<h1>{readability!.title}</h1>, ...rawElements]);
-  console.log(groupedElements);
+  const elements = [<h1>{readability!.title}</h1>, ...rawElements];
 
   const container = <div class="container">
-    {groupedElements
-      .map(elements => <div class="slide">{elements}</div>)}
+    {buildSlides(elements)}
   </div>;
 
   // const slides = groupedElements
@@ -40,6 +37,28 @@ export default function build(html: string): Node {
   // container.append(...slides);
 
   return container;
+}
+
+
+function buildSlides(elements: Node[]): Node[] {
+  const groups = groupElements(elements);
+
+  const usedLayouts: Layout[] = [];
+  const slides = [];
+
+  for (const groupedElements of groups) {
+    const layout = LAYOUTS.find(layout =>
+        layout.isApplicable(groupedElements) && layout !== usedLayouts[usedLayouts.length - 1],
+      )
+      || LAYOUTS.find(layout =>
+        layout.isApplicable(groupedElements)
+      )!;
+
+    usedLayouts.push(layout);
+    slides.push(layout.apply(groupedElements));
+  }
+
+  return slides;
 }
 
 function groupElements(elements: Node[]): Node[][] {
@@ -67,3 +86,38 @@ function groupElements(elements: Node[]): Node[][] {
 function sum(array: number[]) {
   return array.reduce((partialSum, value) => partialSum + value, 0);
 }
+
+type Layout = {
+  isApplicable: (nodes: Node[]) => boolean
+  apply: (nodes: Node[]) => Node
+}
+
+const LAYOUTS: Layout[] = [
+  {
+    isApplicable: nodes => nodes.length === 2,
+    apply: nodes =>
+      <div class="slide bg-blue-500 px-2 gap-y-3">
+        <div class="bg-white mr-4 px-6 py-5 rounded-xl">
+          {nodes[0]}
+        </div>
+        <div class="bg-white ml-4 px-6 py-5 rounded-xl">
+          {nodes[1]}
+        </div>
+      </div>,
+  },
+  {
+    isApplicable: nodes => nodes.length === 1,
+    apply: nodes =>
+      <div class="slide bg-fuchsia-700 text-2xl text-white px-4 leading-8">
+        {nodes}
+      </div>
+  },
+  {
+    isApplicable: () => true,
+    apply: nodes =>
+      <div class="slide px-4">
+        {nodes}
+      </div>,
+  },
+];
+
