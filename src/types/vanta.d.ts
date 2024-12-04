f15d51bd702beebddcc774c07f56d80beca93a6f
Gleb Rusiaev <rusyaew@pm.me>
2024-12-05 01:57:12 +0200

    vanta types

---
+++ src/types/vanta.d.ts
@@ -0,0 +1,22 @@
+declare module 'vanta/dist/vanta.fog.min' {
+    interface VantaFogOptions {
+        el: string | HTMLElement;
+        THREE: typeof THREE;
+        mouseControls?: boolean;
+        touchControls?: boolean;
+        gyroControls?: boolean;
+        minHeight?: number;
+        minWidth?: number;
+        highlightColor?: string;
+        midtoneColor?: string;
+        lowlightColor?: string;
+        baseColor?: string;
+        blurFactor?: number;
+        speed?: number;
+        zoom?: number;
+    }
+
+    const FOG: (options: VantaFogOptions) => void;
+    
+    export default FOG;
+}
