812b11ac6650e55b7eb98196b380cecfa229f168
Artem Ivanov <art.ivanov@jetbrains.com>
2025-01-13 22:49:28 +0200

    Split lib and app

---
+++ reader-app/vite.config.ts
@@ -0,0 +1,35 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [VitePWA({
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'Reader',
+      short_name: 'Reader',
+      description: 'Convert any article to an engaging narrative',
+      theme_color: '#f59e0b',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
