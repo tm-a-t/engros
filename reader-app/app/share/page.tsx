cac3261178bd13b41de73dbd0f7597e803884b73
Artem Ivanov <art.ivanov@jetbrains.com>
2025-01-15 21:53:57 +0200

    fix share page

--- reader-app/app/share/page.tsx
+++ reader-app/app/share/page.tsx
@@ -1,6 +1,10 @@
 import {redirect} from 'next/navigation';
 
-export default async function Share({searchParams}: { searchParams: Promise<{ link: string }> }) {
-  const {link} = await searchParams;
-  redirect('/' + decodeURI(link));
+export default async function Share(
+  {searchParams}:
+    { searchParams: Promise<{ [key: string]: string | string[] | undefined }> },
+) {
+  const params = await searchParams;
+  const link = Array.isArray(params.link) ? params.link[0] : params.link;
+  redirect('/' + decodeURI(link || ''));
 }
