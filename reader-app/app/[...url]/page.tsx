5021c9db731138e7536af5d0bf6a36468baa0c16
Artem Ivanov <art.ivanov@jetbrains.com>
2025-01-15 19:10:53 +0200

    fix url encoding

--- reader-app/app/[...url]/page.tsx
+++ reader-app/app/[...url]/page.tsx
@@ -5,7 +5,7 @@ export default async function Page({params}: { params: Promise<{ url: string[] }
   if (['https%3A', 'http%3A'].includes(urlParts[0])) {
     urlParts.shift();
   }
-  const url = 'https://' + urlParts.join('/');
+  const url = 'https://' + urlParts.map(decodeURIComponent).join('/');
 
   const response = await fetch(url);
   const text = await response.text();
