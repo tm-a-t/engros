1866d09641cc16e941c939239d1719dccf381278
Artem Ivanov <art.ivanov@jetbrains.com>
2025-05-16 16:16:33 +0300

    Fix

--- reader-app/components/rss.tsx
+++ reader-app/components/rss.tsx
@@ -4,7 +4,7 @@ import RSSPoolClient from './rss-client';
 /* ──────────────────────────────────────────────────────────────────────────
    Server component to fetch initial data
 ────────────────────────────────────────────────────────────────────────── */
-export default function RSSPool({ initialLimit = 5 }: { initialLimit?: number } = {}) {
+export default function RSSPool({ initialLimit = 3 }: { initialLimit?: number } = {}) {
     // const initialPosts = await buildPool(0, initialLimit);
 
     return <RSSPoolClient initialLimit={initialLimit} />;
